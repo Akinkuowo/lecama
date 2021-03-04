fd16f71dd7ecfec6e13a24a5f18ad1c3d6c5722e
Oluwaseun <akinseun1990@gmail.com>
2021-03-04 14:21:22 +0100

    adding compopents folder and updating App.js file

--- component/Register.js
+++ component/Register.js
@@ -1,35 +1,38 @@
 
 import React, { useState } from 'react';
 
-import { StyleSheet, View, Text, TextInput, Image, TouchableOpacity } from 'react-native';
-import Register from './component/Register';
+import { StyleSheet, View, Text, TextInput, Image, TouchableOpacity, Touchable } from 'react-native';
 
 
-export default function App() {
-  const [name, setName] = useState('seun');  
+export default function Register() {
+//   const [name, setName] = useState('seun');  
   return (
     <View style={styles.container}>
         <View>
-          <Image style={styles.image} source={require('./assets/logo.png')} />
+          <Image style={styles.image} source={require('../assets/logo.png')} />
         </View>
         <View>    
-            <Text style={styles.textContainer}>Login to Lecama</Text>
+            <Text style={styles.textContainer}>Register</Text>
         </View>
         <View>    
-            <TextInput style={styles.input} placeholder='Username' onChangeText={(val)=> setName(val)}/>
+            <TextInput style={styles.input} placeholder='Enter Your Name' onChangeText={(val)=> setName(val)}/>
         </View>
         <View>    
-            <TextInput style={styles.input} placeholder='Password'/>
+            <TextInput style={styles.input} placeholder='Enter Your Email' onChangeText={(val)=> setName(val)}/>
         </View>
+        <View>    
+            <TextInput style={styles.input} placeholder='Enter Your Password'/>
+        </View>
+        <View>    
+            <TextInput style={styles.input} placeholder='Confirm Your Password'/>
+        </View>
+
         <TouchableOpacity  onPress={()=> alert('akinkuowo')}>
-            <Text style={styles.login}>Login</Text>
+            <Text style={styles.register}>Register</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.forgetPassword}>
-          <Text >Forget Password?</Text>
-        </TouchableOpacity>
-        <View></View>
+        
         <View style={styles.Account}>
-          <Text >Dont't have an Account? <Text style={styles.Register}><TouchableOpacity> Register Now</TouchableOpacity></Text></Text>
+          <Text >Already have an Account? <Text style={styles.Register}><TouchableOpacity> Login</TouchableOpacity></Text></Text>
         </View>
     </View>
   );
@@ -67,13 +70,14 @@ const styles = StyleSheet.create({
     color: 'blue',
     fontSize: 16
   },
-  login: {
+register: {
     backgroundColor: 'green',
     width: 300,
     paddingTop: 10,
     paddingBottom: 10,
     paddingLeft: 40,
     paddingRight: 40,
+    marginBottom: 20,
     textAlign: 'center',
     color: 'white'
   },
